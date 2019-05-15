f28606c5ae573c5aedb2c1883fa97381db60a5e8
Patrick Heflin <patrickslx@yahoo.com>
2019-05-15 08:31:36 -0500

    *

--- server.js
+++ server.js
@@ -91,22 +91,14 @@ app.get('/search/:location/:search_term', (req, res) => {
     })
 })
 
-//Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-//production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
+  //Set static folder
+  app.use(express.static('client/build'))
+
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-})
 
 const port = process.env.PORT || 3001;
 
