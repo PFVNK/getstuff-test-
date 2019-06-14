6ccc4134acb824ba7f1b6663bc2eb8c19b804706
Patrick Heflin <patrickslx@yahoo.com>
2019-06-14 15:46:49 -0500

    *

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.get('/search/:location/:search_term', (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
