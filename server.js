ecba48bcf464845d4a562abafbfce5b932271069
Patrick Heflin <patrickslx@yahoo.com>
2019-06-15 11:53:50 -0500

    *

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.get('/search/:location/:search_term', (req, res) => {
 
   const url = `https://${location}.craigslist.org/search/sso?sort=date&query=${search_term}&hasPic=1`
 
+  console.log(url)
+
   fetch(url)
     .then(res => res.text())
     .then(body => {
@@ -67,9 +69,6 @@ app.get('/search/:location/:search_term', (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-
-
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
